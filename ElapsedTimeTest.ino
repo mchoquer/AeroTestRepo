f00591a4faf3a609f7cea6ab5eaef6c739a4b8a5
mchoquer <michaelachoquer@gmail.com>
2016-03-09 19:54:14 -0800

    Added comment to test importing to Arduino

--- ElapsedTimeTest.ino
+++ ElapsedTimeTest.ino
@@ -1,7 +1,7 @@
 // RTC Elapsed Time and Arduino I2C Test -- Master Code
 #include <Wire.h>
 #include "RTClib.h"
-
+//Testing to see if possible to import edited code on Git to Arduino
 long currentTime = 0;
 long prevTime = 0;
 const int ledPin = 13;
